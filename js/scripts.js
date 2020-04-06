6ce8308fcb012f53249adba78f65afea43faee07
Dominic Montelongo <padma@email.com>
2020-04-06 02:29:21 -0700

    Add function with unspecified var values

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).submit(function() {
+    $("form#formOne").submit(function(event) {
+        event.preventDefault();
+        var question = parseInt($("").val());
+        var question = parseInt($("").val());
+        var question = parseInt($("").val());
+        var question = parseInt($("").val());
+        var question = parseInt($("").val());
+    })
+})
