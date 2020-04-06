ef89c85c16ccf06dd90cfffe4a461c1a61c6b2da
Dominic Montelongo <padma@email.com>
2020-04-06 02:35:42 -0700

    Add better identifying names to variables and call out html values with jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 $(document).submit(function() {
     $("form#formOne").submit(function(event) {
         event.preventDefault();
-        var question = parseInt($("").val());
-        var question = parseInt($("").val());
-        var question = parseInt($("").val());
-        var question = parseInt($("").val());
-        var question = parseInt($("").val());
+        var question1Input = parseInt($("#question1").val());
+        var question2Input = parseInt($("#question2").val());
+        var question3Input = parseInt($("#question3").val());
+        var question4Input = parseInt($("#question4").val());
+        var question5Input = parseInt($("#question5").val());
+
+        var score = question1 + question2 + question3 + question4 + question5;
+
+
     })
 })
